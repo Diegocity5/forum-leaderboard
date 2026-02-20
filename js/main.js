2f1ed1283ebcabd41951886f5b489b2045c13c89
Diegocity5 <icotecc@gmail.com>
2026-02-20 10:03:22 -0500

    4: Declare funcion asincrona usando la palabra clave async dado a que traer datos desde una api es un proceso asincrono o paralelamente al flujo tracional

--- js/main.js
+++ js/main.js
@@ -3,3 +3,8 @@ const forumTopicUrl  = "https://forum.freecodecamp.org/t/";
 const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 const postsContainer = document.getElementById('posts-container');
+
+//Funcion que se ejecuta independiente del flujo principal para traer datos.
+const fetchData = async ()=>{
+    
+}
