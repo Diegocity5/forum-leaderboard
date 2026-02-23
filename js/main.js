533743c1662ff51f49a366f00a1aff6859b21a92
Diegocity5 <icotecc@gmail.com>
2026-02-23 14:15:59 -0500

    22: Declare la funcion timeAgo() que tomo como argumento el tiempo transcurrido en formato ISO 8601  para guardarlo creando esa fecha en lastPost y currenTime tiempo actual dado que necesitaremos esto para obtener el tiempo transcurrido

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@ const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 const postsContainer = document.getElementById('posts-container');
 
+//Funcion encargada de calcular el tiempo transcurrido de la publicación.
+function timeAgo(time){
+    const currentTime = new Date();
+    const lastPost = new Date(time);
+}
+
 //Funcion que se ejecuta independiente del flujo principal para traer datos.
 const fetchData = async ()=>{
     try{
