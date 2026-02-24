d52fa5d1347849fd7d699bc876b648b4432a4cab
Diegocity5 <icotecc@gmail.com>
2026-02-24 12:15:24 -0500

    26: Interpole la funcion viewCount(views) para que se interprete como expresion usando la logica planteada de los numeros de vistas de redes sociales

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ const showLatestPosts = (data)=>{
             <td class="post-title">${title}</td>
             <td></td>
             <td>${posts_count - 1}</td>
-            <td>${views}</td>
+            <td>${viewCount(views)}</td>
             <td>${timeAgo(bumped_at)}</td>
         </tr>`
     }).join('');
